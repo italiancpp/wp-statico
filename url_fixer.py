9432bad2b2f7cad0ce987fa12bc0ff1a99cc36d4
stefano <you@example.com>
2020-09-12 11:40:26 +0200

    Aggiungo pure il tool per cambiare gli url e tenerli relativi. Fa schifo, ma fa il suo mestiere.

---
+++ url_fixer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+def read_parameters():
+	return os.path.abspath(sys.argv[1]), sys.argv[2], sys.argv[3]
+	
+def determine_depth(file_path):
+	return file_path.count('/')
+
+
+def crawl_files(root_folder, search_url, replace_url):
+	base_depth = determine_depth(root_folder)
+	
+	for subdir, dirs, files in os.walk(root_folder):
+		for file in files:
+			file_path = os.path.join(subdir, file)
+			depth_below_root = determine_depth(file_path) - base_depth
+			
+			print (file_path + " -- " + str(depth_below_root))
+			scan_replace(search_url, replace_url, file_path, depth_below_root)
+			
+def scan_replace(search_url, replace_url, file_path, depth_below_root):
+	web_page = open(file_path, "r")
+	text = web_page.read()
+	
+	if search_url in text:
+		print ("Found in " + file_path)
+		
+		relative_prefix = "../" * (depth_below_root -1)
+		
+		text = text.replace(search_url, relative_prefix + replace_url)
+		
+		web_page.close()
+		web_page = open(file_path, "w")
+		web_page.write(text)
+		web_page.close()
+
+
+
+if __name__ == "__main__":
+	root_folder, search_url, replace_url = read_parameters()
+	print("Running in " + root_folder)
+	crawl_files(root_folder, search_url, replace_url)
