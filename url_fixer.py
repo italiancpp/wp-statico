d231ad04ffff7942fed793fea0deada790dad79f
stefano <you@example.com>
2020-09-19 11:13:58 +0200

    Corretti alcuni link al chi siamo.

--- url_fixer.py
+++ url_fixer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3.6
 
 import sys
 import os
@@ -12,29 +12,35 @@ def determine_depth(file_path):
 def crawl_files(root_folder, search_url, replace_url):
 	base_depth = determine_depth(root_folder)
 	
+	count_replacements = 0
 	for subdir, dirs, files in os.walk(root_folder):
 		for file in files:
-			file_path = os.path.join(subdir, file)
-			depth_below_root = determine_depth(file_path) - base_depth
-			
-			print (file_path + " -- " + str(depth_below_root))
-			scan_replace(search_url, replace_url, file_path, depth_below_root)
+			if file.endswith("html"):
+				file_path = os.path.join(subdir, file)
+				depth_below_root = determine_depth(file_path) - base_depth
+				
+				print (file_path + " -- " + str(depth_below_root))
+				count_replacements +=scan_replace(search_url, replace_url, file_path, depth_below_root)
+	print (str(count_replacements) + "sostituzioni")
 			
 def scan_replace(search_url, replace_url, file_path, depth_below_root):
 	web_page = open(file_path, "r")
 	text = web_page.read()
 	
 	if search_url in text:
-		print ("Found in " + file_path)
+		print ("XFound in " + str(file_path))
 		
 		relative_prefix = "../" * (depth_below_root -1)
-		
 		text = text.replace(search_url, relative_prefix + replace_url)
 		
 		web_page.close()
 		web_page = open(file_path, "w")
 		web_page.write(text)
 		web_page.close()
+		
+		return 1
+		
+	return 0
 
 
 
